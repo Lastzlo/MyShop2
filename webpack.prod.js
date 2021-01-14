5f1b14d8f430141180f34689e1245f651205c81b
Lastzlo <z.last.zlo@gmail.com>
2021-01-14 20:11:19 +0200

    refactor(webpack.prod.js): add CleanWebpackPlugin, set output

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,16 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+//импортировали плагин для очистки
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'src', 'main', 'resources', 'static', 'js'),
+    },
 });
