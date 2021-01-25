656a9939ef0434d2efd9416f60f1516cd88fed22
Lastzlo <z.last.zlo@gmail.com>
2021-01-25 22:28:01 +0200

    feat (create-directory-before.sql) add directory with type BRAND_LIST

--- src/test/resources/create-directory-before.sql
+++ src/test/resources/create-directory-before.sql
@@ -5,6 +5,8 @@ insert into linked_directory(id, directory_type, name) values
 (1, 'PARAMETER', 'memory_size');
 insert into linked_directory(id, directory_type, name) values
 (2, 'PARAMETER', 'color');
+insert into linked_directory(id, directory_type, name) values
+(4, 'BRAND_LIST', 'color');
 
 insert into product(id, product_name) values
 (3, 'Apple iPhone 10');
