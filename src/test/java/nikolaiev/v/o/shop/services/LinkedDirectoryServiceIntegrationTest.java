d2a73423e2534a1c4884bec7e32dadb80b8f7340
Lastzlo <z.last.zlo@gmail.com>
2021-01-25 19:14:15 +0200

    test (LinkedDirectoryServiceIntegrationTest.java) add two tests
    LinkedDirectoryServiceIntegrationTest.addDirectoriesToProductButDirectoriesNotFoundTest
    LinkedDirectoryServiceIntegrationTest.addDirectoriesToProductTest

---
+++ src/test/java/nikolaiev/v/o/shop/services/LinkedDirectoryServiceIntegrationTest.java
@@ -0,0 +1,90 @@
+package nikolaiev.v.o.shop.services;
+
+import nikolaiev.v.o.shop.domain.LinkedDirectory;
+import nikolaiev.v.o.shop.domain.Product;
+import nikolaiev.v.o.shop.repos.LinkedDirectoryRepo;
+import nikolaiev.v.o.shop.repos.ProductRepo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@SpringBootTest
+//Спринг пытаеться автоматически создать структуру
+//классов поторая подменяет слой MVC
+//все будет проходить в фейковом окружении
+@AutoConfigureMockMvc
+//аннотация указывает на новый файл с настройками
+@TestPropertySource("/application-test.properties")
+class LinkedDirectoryServiceIntegrationTest {
+
+    @Autowired
+    private LinkedDirectoryService directoryService;
+
+    @Test
+    //перед тестом выполнить очистку и заполнение БД
+    @Sql(value = {"/create-directory-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    //после теста выполнить очистку БД
+    @Sql(value = {"/create-directory-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void addDirectoriesToProduct () {
+        //given
+        final LinkedDirectory directory1 = new LinkedDirectory () {{
+            this.setId (1l);
+        }};
+        final LinkedDirectory directory2 = new LinkedDirectory () {{
+            this.setId (2l);
+        }};
+
+        final Set<LinkedDirectory> directories = new HashSet<LinkedDirectory> (){{
+            add (directory1);
+            add (directory2);
+        }};
+
+        Product product = new Product (){{
+            this.setDirectories (new HashSet<> ());
+        }};
+
+        final Product actualProduct = directoryService.addDirectoriesToProduct (directories, product);
+
+        Assertions.assertEquals (2, actualProduct.getDirectories ().size ());
+    }
+
+    @Test
+    public void addDirectoriesToProductButDirectoriesNotFound () {
+        //given
+        final LinkedDirectory directory1 = new LinkedDirectory () {{
+            this.setId (55l);
+        }};
+        final LinkedDirectory directory2 = new LinkedDirectory () {{
+            this.setId (66l);
+        }};
+
+        final Set<LinkedDirectory> directories = new HashSet<LinkedDirectory> (){{
+            add (directory1);
+            add (directory2);
+        }};
+
+        Product product = new Product (){{
+            this.setDirectories (new HashSet<> ());
+        }};
+
+        final Product actualProduct = directoryService.addDirectoriesToProduct (directories, product);
+
+        Assertions.assertEquals (0, actualProduct.getDirectories ().size ());
+    }
+
+
+}
+
