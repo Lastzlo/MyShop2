5150c0a87c8f591ab96db19bc3fc5bccef9ba7dd
Lastzlo <z.last.zlo@gmail.com>
2021-01-25 15:13:05 +0200

    feat (DirectoryController.java) add logger to DirectoryController.getCore

--- src/main/java/nikolaiev/v/o/shop/controller/DirectoryController.java
+++ src/main/java/nikolaiev/v/o/shop/controller/DirectoryController.java
@@ -5,6 +5,8 @@ import nikolaiev.v.o.shop.domain.Product;
 import nikolaiev.v.o.shop.domain.Views;
 import nikolaiev.v.o.shop.services.LinkedDirectoryService;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import java.util.Set;
 @RestController
 @RequestMapping("directory")
 public class DirectoryController {
+    private static final Logger logger = LogManager.getLogger(DirectoryController.class);
 
     @Autowired
     private LinkedDirectoryService directoryService;
@@ -30,6 +33,7 @@ public class DirectoryController {
     @GetMapping("/getCore")
     @JsonView(Views.FullLinkedDirectory.class)
     public ResponseEntity<LinkedDirectory> getCore(){
+        logger.info("DirectoryController.getCore () is executed");
         try {
             return ResponseEntity.ok()
                     .location((new URI ("/directory/getCore")))
