ef31827eeef1f662cf6fbb7807cc3e41d892e471
Lastzlo <z.last.zlo@gmail.com>
2021-01-26 10:58:56 +0200

    refactor (DirectoryType.java) delete BRAND_LIST and BRAND

--- src/main/java/nikolaiev/v/o/shop/domain/DirectoryType.java
+++ src/main/java/nikolaiev/v/o/shop/domain/DirectoryType.java
@@ -3,9 +3,6 @@ package nikolaiev.v.o.shop.domain;
 public enum DirectoryType {
     CATEGORY_LIST,      //директория высшего уровня, ядро
     CATEGORY,
-    BRAND_LIST,
-    BRAND,
     PARAMETER_LIST,
-    PARAMETER,
-    PARAMETER_VALUE
+    PARAMETER
 }
