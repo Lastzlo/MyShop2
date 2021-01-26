e3df6648c748e1678c598f64ea03c239a11d2ff4
Lastzlo <z.last.zlo@gmail.com>
2021-01-26 21:31:08 +0200

    refactor (create-product-after.sql and create-product-before.sql) change product id to 3 and add 3 directories

--- src/test/resources/create-product-after.sql
+++ src/test/resources/create-product-after.sql
@@ -1,1 +1,2 @@
+delete from linked_directory;
 delete from product;
--- src/test/resources/create-product-before.sql
+++ src/test/resources/create-product-before.sql
@@ -1,6 +1,14 @@
+delete from linked_directory;
 delete from product;
 
+insert into linked_directory(id, directory_type, name) values
+(1, 'PARAMETER', 'memory_size');
+insert into linked_directory(id, directory_type, name) values
+(2, 'PARAMETER', 'color');
+insert into linked_directory(id, directory_type, name) values
+(4, 'PARAMETER_LIST', 'color');
+
 insert into product(id, product_name) values
-(1, 'Apple iPhone 10');
+(3, 'Apple iPhone 10');
 
 alter sequence hibernate_sequence restart with 10;
