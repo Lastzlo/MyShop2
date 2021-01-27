97d1a9134481764faf3ba5083c403befe43a65be
Lastzlo <z.last.zlo@gmail.com>
2021-01-27 15:53:42 +0200

    refactor (LinkedDirectoryPredicates.getPredicateForAddDirectoryToOtherDirectory) rename method and add javadoc

--- src/main/java/nikolaiev/v/o/shop/Predicates/LinkedDirectoryPredicates.java
+++ src/main/java/nikolaiev/v/o/shop/Predicates/LinkedDirectoryPredicates.java
@@ -5,11 +5,15 @@ import nikolaiev.v.o.shop.domain.LinkedDirectory;
 
 import java.util.function.Predicate;
 
+
+/**
+ * Условия для LinkedDirectory
+ */
 public class LinkedDirectoryPredicates {
     /**
-     * @return условие которое должна выполнить директория
+     * @return условие при котором тип директории подходит чтобы ее добавить к другим директориям
      */
-    public static Predicate<LinkedDirectory> getDirectoryPredicate (){
+    public static Predicate<LinkedDirectory> getPredicateForAddDirectoryToOtherDirectory (){
         //условие
         Predicate<LinkedDirectory> isDirectorySuitable = linkedDirectory -> {
             return linkedDirectory.getDirectoryType ().equals (DirectoryType.PARAMETER.toString ());
