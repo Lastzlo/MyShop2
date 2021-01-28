aa96cae9b81c98d862e965acd8f22667fc7b2d71
Lastzlo <z.last.zlo@gmail.com>
2021-01-28 22:27:50 +0200

    feat (LinkedDirectoryUtils.java) add 3 methods
    dislinkDirectoriesButProduct (Set<LinkedDirectory> directoriesToDislink, Set<LinkedDirectory> directories, Product product)
    
    dislinkDirectoriesButProduct (Set<LinkedDirectory> directories, Product product)
    
    dislinkDirectoryWithDirectoriesButProduct (LinkedDirectory directoryToDislink, Set<LinkedDirectory> directories, Product product)

--- src/main/java/nikolaiev/v/o/shop/util/LinkedDirectoryUtils.java
+++ src/main/java/nikolaiev/v/o/shop/util/LinkedDirectoryUtils.java
@@ -53,6 +53,126 @@ public class LinkedDirectoryUtils {
         );
     }
 
+
+    /**
+     * Убрать у директорий directories связи с директориями directoriesToDislink
+     * не смотря на то что их может связывать product
+     *
+     * @param directories
+     * @param directoriesToDislink
+     * @param product
+     */
+    public static void dislinkDirectoriesButProduct (Set<LinkedDirectory> directoriesToDislink, Set<LinkedDirectory> directories, Product product) {
+        directoriesToDislink.forEach (
+                directoryToDislink->{
+                    dislinkDirectoryWithDirectoriesButProduct (directoryToDislink, directories, product);
+                }
+        );
+    }
+
+    /**
+     * Убрать связи между директориями directories
+     * не смотря на то что их может связывать product
+     *
+     * @param directories
+     * @param product
+     */
+    public static void dislinkDirectoriesButProduct (Set<LinkedDirectory> directories, Product product) {
+        Set<LinkedDirectory> directoriesToDislink = new HashSet<LinkedDirectory> (){{
+            addAll (directories);
+        }};
+
+        directoriesToDislink.forEach (
+                directoryToDislink->{
+                    dislinkDirectoryWithDirectoriesButProduct (directoryToDislink, directories, product);
+                }
+        );
+    }
+
+    /**
+     * Убрать у директорий directories связи с директорией directoryToDislink
+     * не смотря на то что их может связывать product
+     *
+     * @param directories
+     * @param directoryToDislink
+     * @param product
+     */
+    private static void dislinkDirectoryWithDirectoriesButProduct (LinkedDirectory directoryToDislink, Set<LinkedDirectory> directories, Product product) {
+
+        if (
+                //если directoryToDislink не имеет привязаных продуктов
+                directoryToDislink.getProducts ().size () == 0
+                ||(
+                        //или имеет один продукт который product
+                        directoryToDislink.getProducts ().size () == 1
+                        && directoryToDislink.getProducts ().contains (product)
+                )
+        ) {
+//                        System.out.println ("directoryToDislink не имеет привязаных продуктов, можно удалялть связи с directories");
+
+            directories.forEach (
+                    directory -> {
+
+//                                    System.out.println ("//удалили directoryToDislink с oldNeededDirectoryId");
+                        directory.getRelatedDirectories ().remove (directoryToDislink);
+                        directory.getRelatedDirectoryIds ().remove (directoryToDislink.getId ());
+
+//                                    System.out.println ("//удалили oldNeededDirectoryId с directoryToDislink");
+                        directoryToDislink.getRelatedDirectories ().remove (directory);
+                        directoryToDislink.getRelatedDirectoryIds ().remove (directory.getId ());
+
+                    }
+
+            );
+
+        } else {
+//                        System.out.println ("directoryToDislink имеет ("+directoryToDislink.getProductsCount ()+") привязаных продуктов");
+
+            directories.forEach (
+                    directory -> {
+                        //встречаеться ли хоть один раз directory в directoryToDislink.getProducts.Product.getDirectories
+                        boolean isOldNeededDirectoryInDirectoryToDeleteProductsDirectories = false;
+
+                        for (Product product1 : directoryToDislink.getProducts ()
+                        ) {
+                            if (
+                                    !product1.equals (product) &&
+                                    product1.getDirectories ().contains (directory)
+                            ) {
+                                isOldNeededDirectoryInDirectoryToDeleteProductsDirectories = true;
+
+                                break;
+                            }
+
+                        }
+
+                        //встречаеться ли хоть один раз directory в directoryToDislink.getProducts.Product.getDirectories
+                        if (isOldNeededDirectoryInDirectoryToDeleteProductsDirectories) {
+//                                        System.out.println ("isOldNeededDirectoryInDirectoryToDeleteProductsDirectories");
+//                                        System.out.println ("Да встречаеться directory в directoryToDislink.getProducts.Product.getDirectories");
+                        } else {
+//                                        System.out.println ("!isOldNeededDirectoryInDirectoryToDeleteProductsDirectories");
+//                                        System.out.println ("Нет, не встречаеться directory в directoryToDislink.getProducts.Product.getDirectories");
+
+//                                        System.out.println ("directory = "+directory);
+//                                        System.out.println ("directoryToDislink = "+directoryToDislink);
+
+//                                        System.out.println ("//удалили directoryToDislink с oldNeededDirectoryId");
+                            directory.getRelatedDirectories ().remove (directoryToDislink);
+                            directory.getRelatedDirectoryIds ().remove (directoryToDislink.getId ());
+
+//                                        System.out.println ("//удалили oldNeededDirectoryId с directoryToDislink");
+                            directoryToDislink.getRelatedDirectories ().remove (directory);
+                            directoryToDislink.getRelatedDirectoryIds ().remove (directory.getId ());
+                        }
+
+                    }
+            );
+
+
+        }
+    }
+
     /**
      * Убрать у директории directoryToDislink связи с директориями directories
      *
