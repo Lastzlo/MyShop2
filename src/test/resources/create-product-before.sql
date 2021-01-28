3e585d6574eab03bf5755e37bc0a5a9f94d16159
Lastzlo <z.last.zlo@gmail.com>
2021-01-28 14:06:25 +0200

    fix (create-product-after.sql and create-product-before.sql) add clear some tables
    delete from directories_associated_with_this_directory;
    delete from linked_directory_related_directories;
    
    delete from linked_directory_products;
    delete from product_directories;

--- src/test/resources/create-product-before.sql
+++ src/test/resources/create-product-before.sql
@@ -1,3 +1,10 @@
+delete from directories_associated_with_this_directory;
+delete from linked_directory_related_directories;
+
+delete from linked_directory_products;
+delete from product_directories;
+
+
 delete from linked_directory;
 delete from product;
 
